091812156ac7a2bb18bdef93a3ba409e9c44d641
k.kishimoto <k.kishimoto@tehno-nore.jp>
2026-03-03 14:09:24 +0900

    ドメインモデル用のプロジェクトと単体テストプロジェクトの追加

---
+++ MyTodo.Domain/TodoItem.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+
+namespace MyTodo.Domain.Todo
+{
+    public record TodoId(int Value);
+    public record TodoTitle(string Value);
+    public record TodoIsCompleted(bool Value);
+
+    public record TodoItem(TodoId Id, TodoTitle Title, TodoIsCompleted IsCompleted);
+
+    public record TodoItems(IEnumerable<TodoItem> Items)
+    {
+        public TodoItems AllCompleted()
+        {
+            return new TodoItems(Items.Select(item => item with { IsCompleted = new TodoIsCompleted(true) }));
+        }
+    }
+}
+
---
+++ TodoApp.Tests/TodoItemTest.cs
@@ -0,0 +1,19 @@
+﻿
+using MyTodo.Domain.Todo;
+
+namespace TodoApp.Test.Domain;
+public class TodoItemTest
+{
+    [Fact]
+    public void AllCompletedTest()
+    {
+        var items = new TodoItems(
+            [
+                new TodoItem(new TodoId(1), new TodoTitle("Task 1"), new TodoIsCompleted(false)),
+                new TodoItem(new TodoId(2), new TodoTitle("Task 2"), new TodoIsCompleted(false))
+            ]
+        );
+        var completedItems = items.AllCompleted();
+        Assert.All(completedItems.Items, item => Assert.True(item.IsCompleted.Value));
+    }
+}
